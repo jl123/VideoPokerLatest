f08705eaf102049c22728c961ceb551c1d1e6bac
Joseph <jlaos1234@yahoo.com>
2018-04-01 12:46:49 -0700

    Added credits buttons Draw button disappears after cards drawn credit buttons added (partially functional)
    Need:
    implement handVal
    implement credits calculation
    implement buttons for number of credits to play 1-5
    Complete layout and various other GUI components.
    Move game to new class

--- src/VPGui.java
+++ src/VPGui.java
@@ -16,15 +16,20 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class VPGui extends Application {
+   static int betInt = 5;
+   static int credits = 1000;
 
    @Override
    public void start(Stage primaryStage) {
-
       Deck deck = new Deck(true);
       Hand playerHand = new Hand();
       ImageView cardImages[] = new ImageView[Hand.MAX_CARDS];
-      Button btn[] = new Button[Hand.MAX_CARDS];
+      Button cardButton[] = new Button[Hand.MAX_CARDS];
+      Button credits[] = new Button[5];
+
       Image image;
       StackPane root = new StackPane();
       GridPane grid = new GridPane();
@@ -42,16 +47,21 @@ public class VPGui extends Application {
 
          holdDrawLabel[k] = new Label();
          //System.out.println(holdDrawLabel[k].toString());
-         btn[k] = new Button();
-         btn[k].setGraphic(new ImageView(image));
-         btn[k].setOnAction(new JoesEventHandler(playerHand, holdDrawLabel, k));
+         cardButton[k] = new Button();
+         cardButton[k].setGraphic(new ImageView(image));
+         cardButton[k].setOnAction(new JoesEventHandler(playerHand, holdDrawLabel, k));
 
-
-         grid.add(btn[k], k, 0);
+         //CREDITS BUTTONS
+         int credit = k + 1;
+         credits[k] = new Button(String.valueOf(credit));
+         grid.add(cardButton[k], k, 0);
          grid.add(holdDrawLabel[k], k, 1);
-         //root.add(cardImages[k], k, 0);
+         grid.add(credits[k], k, 2);
+         credits[k].setOnAction(new CreditEventsHandler(k+1));
       }
       root.getChildren().add(grid);
+      Button play = new Button("PLAY");
+      grid.add(play, 5, 2);
       Button draw = new Button("DRAW");
       draw.setOnAction(new EventHandler<ActionEvent>()
       {
@@ -67,14 +77,15 @@ public class VPGui extends Application {
                //cardImages[k] = new ImageView();
                cardImages[k].setImage(image);
                //btn[k] = new Button();
-               btn[k].setGraphic(new ImageView(image));
-               //System.out.println(image.toString());
+               cardButton[k].setGraphic(new ImageView(image));
+               System.out.println(image.toString());
+               grid.getChildren().remove(draw);
             }
 
          }
       });
 
-      grid.add(draw,0,2);
+      grid.add(draw,0,3);
       Scene scene = new Scene(root, 900, 1000);
 
       primaryStage.setTitle("Video Poker!");
@@ -86,6 +97,22 @@ public class VPGui extends Application {
       launch(args);
    }
 
+   private static class CreditEventsHandler implements EventHandler<ActionEvent>
+   {
+      int credit;
+      CreditEventsHandler(int credit)
+      {
+         this.credit = credit;
+      }
+
+      @Override
+      public void handle(ActionEvent event)
+      {
+         betInt = credit;
+         System.out.println(betInt);
+      }
+
+   }
    private static class JoesEventHandler implements EventHandler<ActionEvent>
    {
       int cardNum;
@@ -107,7 +134,7 @@ public class VPGui extends Application {
          System.out.println("old val: " + playerHand.switchCard[cardNum]);
          boolean currentValue = playerHand.switchCard[cardNum];
          playerHand.switchCard[cardNum] = !currentValue;
-         //System.out.println("new val: " + playerHand.switchCard[cardNum]);
+         System.out.println("new val: " + playerHand.switchCard[cardNum]);
          if (!playerHand.switchCard[cardNum])
          {
 
